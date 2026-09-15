f4b027d65dc5d06e0c8d5b37ae1e16be9417b2df
Nabil-AZ <nabilazraf06@gmail.com>
2026-09-16 01:34:07 +0700

    Add files via upload

---
+++ coding nabil 6 (operasi aritmatika).py
@@ -0,0 +1,23 @@
+#Nabil
+#operasi aritmatika
+
+#Note: + = penjumlahan
+#      - = pengurangan
+#      * = perkalian
+#     ** = pangkat
+#      / = pembagian (float)
+#     // = pembagian bulat (interger)
+#      % = modulus (sisa pembagian)
+
+x = 3.14
+y = 4
+z = 5
+
+result = round(x) #mengubah nilai menjadi bilangan bulat terdekat (interger)
+#result = abs(x) #mengubah nilai menjadi bilangan positif / nilai mutlak (absolute)
+#result = pow(y, z) #menghitung nilai y pangkat z
+#result = max(x, y, z) #mengambil nilai terbesar dari x, y, z
+#result = min(x, y, z) #mengambil nilai terkecil dari x, y, z
+
+#kalau mau print result, hapus tanda pagar (#) di atas, samping variable result.
+print(result) 
