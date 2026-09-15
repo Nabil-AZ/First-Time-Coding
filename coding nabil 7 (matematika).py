bc182c47b9c149092e751862b71c692208a251b4
Nabil-AZ <nabilazraf06@gmail.com>
2026-09-16 01:51:02 +0700

    Add files via upload

---
+++ coding nabil 7 (matematika).py
@@ -0,0 +1,23 @@
+#Nabil
+#matematika
+
+import math
+
+x = 9
+y = 9.1
+z = 9.9
+
+print(math.pi) #mendapatkan nilai pi
+print(math.e) #mendapatkan nilai e
+
+result = math.sqrt(x) #mendapatkan akar kuadrat dari x
+#misalnya jika x = 9 maka hasilnya adalah 3
+
+#result = math.ceil(y) #mendapatkan pembulatan ke atas dari y
+#misalnya jika y = 9.1 maka hasilnya adalah 10
+
+#result = math.floor(z) #mendapatkan pembulatan ke bawah dari z
+#misalnya jika z = 9.9 maka hasilnya adalah 9
+
+#kalau mau pake fungsi diatas tinggal hapus tanda pagar (#) di atas, sebelah result
+print(result)
